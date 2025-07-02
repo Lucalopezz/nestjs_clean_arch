79209b651741248a9478a405f9760e2ddee1d1eb
Lucas <lucaslops12@gmail.com>
2025-07-02 20:59:05 -0300

    iniciando a entidade user

---
+++ src/users/domain/entities/__tests__/unity/user.entity.spec.ts
@@ -0,0 +1,22 @@
+import { faker } from '@faker-js/faker';
+import { UserEntity, UserProps } from '../../user.entity';
+
+describe('User Entity unit tests', () => {
+  let props: UserProps;
+  let sut: UserEntity;
+  beforeEach(() => {
+    props = {
+      name: faker.person.fullName(),
+      email: faker.internet.email(),
+      password: faker.internet.password(),
+    };
+    sut = new UserEntity(props);
+  });
+
+  it('Constructor should create an instance of User', () => {
+    expect(sut.props.name).toBe(props.name);
+    expect(sut.props.email).toBe(props.email);
+    expect(sut.props.password).toBe(props.password);
+    expect(sut.props.createdAt).toBeDefined();
+  });
+});
