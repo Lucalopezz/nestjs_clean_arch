ffcb3eee3e2d4ecfd6b45ab532e7a1400ed745a4
Lucas <lucaslops12@gmail.com>
2025-07-08 14:00:26 -0300

    feat: implement filtering and sorting methods in UserInMemoryRepository

--- src/users/infrastructure/database/in-memory/repositories/user-in-memory.repository.ts
+++ src/users/infrastructure/database/in-memory/repositories/user-in-memory.repository.ts
@@ -1,6 +1,7 @@
 import { ConflictError } from '@/shared/domain/errors/conflict-error';
 import { NotFoundError } from '@/shared/domain/errors/not-found-error';
 import { InMemorySearchableRepository } from '@/shared/domain/repositories/in-memory-searchable.repository';
+import { SortDirection } from '@/shared/domain/repositories/sheachable-repository-contracts';
 import { UserEntity } from '@/users/domain/entities/user.entity';
 import { UserRepository } from '@/users/domain/repositories/user.repository';
 
@@ -10,8 +11,10 @@ import { UserRepository } from '@/users/domain/repositories/user.repository';
 // A classe UserInMemoryRepository implementa a interface UserRepository, que define métodos específicos
 export class UserInMemoryRepository
   extends InMemorySearchableRepository<UserEntity>
-  implements UserRepository
+  implements UserRepository.Repository
 {
+  sortableFields: string[] = ['name', 'created_at'];
+
   async findByEmail(email: string): Promise<UserEntity> {
     const entity = this.items.find((item) => item.email === email);
     if (!entity) {
@@ -24,5 +27,29 @@ export class UserInMemoryRepository
     if (entity) {
       throw new ConflictError('Email address already used');
     }
+  }
+  protected async applyFilter(
+    items: UserEntity[],
+    filter: UserRepository.Filter,
+  ): Promise<UserEntity[]> {
+    if (!filter) {
+      return items;
+    }
+    return items.filter((item) => {
+      return item.props.name.toLowerCase().includes(filter.toLowerCase());
+    });
+  }
+  // Sobrescreve o método applySort para aplicar a ordenação dos itens
+  // caso o parâmetro sort não seja fornecido.
+  // Se sort for null, aplica a ordenação padrão por createdAt em ordem decrescente.
+  // Caso contrário, aplica a ordenação pelo campo especificado e pela direção fornecida.
+  protected async applySort(
+    items: UserEntity[],
+    sort: string | null,
+    sortDir: SortDirection | null,
+  ): Promise<UserEntity[]> {
+    return !sort
+      ? super.applySort(items, 'createdAt', 'desc')
+      : super.applySort(items, sort, sortDir);
   }
 }
