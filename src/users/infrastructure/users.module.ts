de941969886be2a30fb5fd67361c1b7e4b769bcd
Lucas <lucaslops12@gmail.com>
2025-07-13 15:53:54 -0300

    refactor: update UsersModule to include dependency injection for UserRepository and HashProvider

--- src/users/infrastructure/users.module.ts
+++ src/users/infrastructure/users.module.ts
@@ -2,9 +2,35 @@ import { Module } from '@nestjs/common';
 
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { SignupUseCase } from '../application/usecases/signup.usecase';
+import { UserInMemoryRepository } from './database/in-memory/repositories/user-in-memory.repository';
+import { BcryptjsHashProvider } from '@/shared/application/providers/bcryptjs-hash.provider';
+import { UserRepository } from '../domain/repositories/user.repository';
+import { HashProvider } from '@/shared/application/providers/hash-provider';
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [
+    UsersService,
+    {
+      provide: 'UserRepository', // this is the name, if u want to use in memory or a prisma repo
+      useClass: UserInMemoryRepository,
+    },
+    {
+      provide: 'HashProvider',
+      useClass: BcryptjsHashProvider,
+    },
+    {
+      provide: SignupUseCase.UseCase,
+      // this is the method to inject dependencies into the use case
+      useFactory: (
+        userRepository: UserRepository.Repository,
+        hashProvider: HashProvider,
+      ) => {
+        return new SignupUseCase.UseCase(userRepository, hashProvider);
+      },
+      inject: ['UserRepository', 'HashProvider'],
+    },
+  ],
 })
 export class UsersModule {}
