06854a16c6537e334b70138b49edcc10592c906f
Lucas <lucaslops12@gmail.com>
2025-07-21 08:47:17 -0300

    feat: config validationPipe and implement validation in dtos

--- src/users/infrastructure/dtos/signup.dto.ts
+++ src/users/infrastructure/dtos/signup.dto.ts
@@ -1,7 +1,17 @@
 import { SignupUseCase } from '@/users/application/usecases/signup.usecase';
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class SignupDto implements SignupUseCase.Input {
+  @IsString()
+  @IsNotEmpty()
+  @IsEmail()
   email: string;
+
+  @IsString()
+  @IsNotEmpty()
   password: string;
+
+  @IsString()
+  @IsNotEmpty()
   name: string;
 }
