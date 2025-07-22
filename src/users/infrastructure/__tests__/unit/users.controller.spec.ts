b3a0476c1ea16cd99060d9d3e66db6d5b220f901
Lucas <lucaslops12@gmail.com>
2025-07-22 08:37:27 -0300

    feat: update WrapperDataInterceptor logic and enhance UsersController with AuthService for JWT generation

--- src/users/infrastructure/__tests__/unit/users.controller.spec.ts
+++ src/users/infrastructure/__tests__/unit/users.controller.spec.ts
@@ -2,7 +2,6 @@ import { UsersController } from '../../users.controller';
 import { SignupUseCase } from '@/users/application/usecases/signup.usecase';
 import { SignupDto } from '../../dtos/signup.dto';
 import { UserOutput } from '@/users/application/dtos/user-output.dto';
-import { SigninUseCase } from '@/users/application/usecases/signin.usecase';
 import { SigninDto } from '../../dtos/signin.dto';
 import { UpdateUserUseCase } from '@/users/application/usecases/updateuser.usecase';
 import { UpdateUserDto } from '../../dtos/update-user.dto';
@@ -54,18 +53,21 @@ describe('UsersController unit tests', () => {
   });
 
   it('should authenticate a user', async () => {
-    const output: SigninUseCase.Output = props;
+    const output = 'fake_token';
     const mockSigninUseCase = {
       execute: jest.fn().mockReturnValue(Promise.resolve(output)),
     };
+    const mockAuthService = {
+      generateJwt: jest.fn().mockReturnValue(Promise.resolve(output)),
+    };
     sut['signinUseCase'] = mockSigninUseCase as any;
+    sut['authService'] = mockAuthService as any;
     const input: SigninDto = {
       email: 'a@a.com',
       password: '1234',
     };
-    const presenter = await sut.login(input);
-    expect(presenter).toBeInstanceOf(UserPresenter);
-    expect(presenter).toStrictEqual(new UserPresenter(output));
+    const result = await sut.login(input);
+    expect(result).toEqual(output);
     expect(mockSigninUseCase.execute).toHaveBeenCalledWith(input);
   });
 
