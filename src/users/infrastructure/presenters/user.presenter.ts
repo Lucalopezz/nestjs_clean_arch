2558d37dfdc80ad89a911550ca53a94e6aecea70
Lucas <lucaslops12@gmail.com>
2025-07-23 14:01:23 -0300

    feat: enhance UserPresenter and UsersController with Swagger documentation

--- src/users/infrastructure/presenters/user.presenter.ts
+++ src/users/infrastructure/presenters/user.presenter.ts
@@ -1,12 +1,28 @@
 import { CollectionPresenter } from '@/shared/infrastructure/presenters/collection.presenter';
 import { UserOutput } from '@/users/application/dtos/user-output.dto';
 import { ListUsersUseCase } from '@/users/application/usecases/listusers.usecase';
+import { ApiProperty } from '@nestjs/swagger';
 import { Transform } from 'class-transformer';
 
 export class UserPresenter {
+  @ApiProperty({
+    description: 'The ID of the user',
+  })
   id: string;
+
+  @ApiProperty({
+    description: 'The name of the user',
+  })
   name: string;
+
+  @ApiProperty({
+    description: 'The email of the user',
+  })
   email: string;
+
+  @ApiProperty({
+    description: 'The date of creation of the user',
+  })
   @Transform(({ value }: { value: Date }) => value.toISOString())
   createdAt: Date;
 
