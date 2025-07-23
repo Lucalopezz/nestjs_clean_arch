7398160a85583f9f157cd0c01017fad87ef99d11
Lucas <lucaslops12@gmail.com>
2025-07-23 12:52:32 -0300

    feat: implement AuthGuard to protected private routes

--- src/users/infrastructure/users.controller.ts
+++ src/users/infrastructure/users.controller.ts
@@ -10,6 +10,7 @@ import {
   HttpCode,
   Query,
   Put,
+  UseGuards,
 } from '@nestjs/common';
 import { SignupDto } from './dtos/signup.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
@@ -29,6 +30,7 @@ import {
   UserPresenter,
 } from './presenters/user.presenter';
 import { AuthService } from '@/auth/infrastructure/auth.service';
+import { AuthGuard } from '@/auth/infrastructure/auth.guard';
 
 @Controller('users')
 export class UsersController {
@@ -71,26 +73,29 @@ export class UsersController {
     return UsersController.userToResponse(output);
   }
 
-  @HttpCode(200)
   @Post('login')
+  @HttpCode(200)
   async login(@Body() signinDto: SigninDto) {
     const output = await this.signinUseCase.execute(signinDto);
     return this.authService.generateJwt(output.id);
   }
 
   @Get()
+  @UseGuards(AuthGuard)
   async search(@Query() searchParams: ListUsersDto) {
     const output = await this.listUsersUseCase.execute(searchParams);
     return UsersController.listUsersToResponse(output);
   }
 
   @Get(':id')
+  @UseGuards(AuthGuard)
   async findOne(@Param('id') id: string) {
     const output = await this.getUserUseCase.execute({ id });
     return UsersController.userToResponse(output);
   }
 
   @Put(':id')
+  @UseGuards(AuthGuard)
   async update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     const output = await this.updateUserUseCase.execute({
       id,
@@ -100,6 +105,7 @@ export class UsersController {
   }
 
   @Patch(':id')
+  @UseGuards(AuthGuard)
   async updatePassword(
     @Param('id') id: string,
     @Body() updatePasswordDto: UpdatePasswordDto,
@@ -111,8 +117,9 @@ export class UsersController {
     return UsersController.userToResponse(output);
   }
 
-  @HttpCode(204)
   @Delete(':id')
+  @HttpCode(204)
+  @UseGuards(AuthGuard)
   async remove(@Param('id') id: string) {
     await this.deleteUserUseCase.execute({ id });
   }
