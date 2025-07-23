67a88a30dffdbeba3b83a51fe21b07ad81ab65aa
Lucas <lucaslops12@gmail.com>
2025-07-23 13:17:38 -0300

    feat: add swagger an configure it to start the documentation

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,30 @@ import {
 } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 import { applyGlobalConfig } from './global-config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter(),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('Nest Clean Architecture API')
+    .setDescription('API documentation for the Nest Clean Architecture project')
+    .setVersion('1.0')
+    .addBearerAuth({
+      description: 'Use JWT to authorize the access',
+      name: 'Authorization',
+      scheme: 'Bearer',
+      type: 'http',
+      in: 'Header',
+    })
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   applyGlobalConfig(app);
   await app.listen(3000, '0.0.0.0');
 }
